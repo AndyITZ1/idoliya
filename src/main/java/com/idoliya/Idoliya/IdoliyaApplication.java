d70bc31718d1e265a5d1c1b3e9b345d21fefd1f8
Andy Tran <105564473+AndyITZ1@users.noreply.github.com>
2023-05-09 21:41:50 -0400

    first commit

---
+++ src/main/java/com/idoliya/Idoliya/IdoliyaApplication.java
@@ -0,0 +1,13 @@
+package com.idoliya.Idoliya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdoliyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdoliyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idoliya/Idoliya/IdoliyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idoliya.Idoliya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdoliyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
